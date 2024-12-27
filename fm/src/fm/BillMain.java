ef149ad73bca79e264624f81c00b6979a5cdb96f
dzf <1595856469@qq.com>
2024-12-27 11:59:16 +0800

    添加功能

---
+++ fm/src/fm/BillMain.java
@@ -0,0 +1,14 @@
+package bill2;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Scanner;
+public class BillMain {
+	public static void main(String[] args) {
+        BillManagementSystem system = new BillManagementSystem();
+        system.run();
+    }
+
+
+}
+
